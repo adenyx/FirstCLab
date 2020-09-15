c14172929a921030634288ff489ac0a69ec673ca
adenyx <59793388+adenyx@users.noreply.github.com>
2020-09-15 17:20:11 +0300

    Added fourth and fifth lab condition Added some notes of fifth lab

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#define N 7
+#define _CRT_SECURE_NO_WARNINGS
 
 //Дана действительная квадратная матрица порядка n, все элементы которой различны.
 //Найти наибольший элемент среди стоящих на главной и побочной диагоналях и поменять его местами с элементом,
@@ -187,9 +189,61 @@ int third() {
 
 }
 
+//Дана действительная квадратная матрица порядка n. Найти наименьшее
+//из значений элементов, расположенных в заштрихованной части матрицы.
+//(сверху, снизу и слева между диагоналями)((сами диагонали не учитываем))
+int fourth() {
+    int array[N][N];
+    srand(time(NULL));
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) {
+            array[i][j] = rand() % 100;
+        }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d\t", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    int min = array[0][1];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            if (j > i && j < N - i - 1) {
+                if (array[i][j] < min) {
+                    min = array[i][j];
+                }
+                int k = N - i -1;
+                if (array[k][j] < min) {
+                    min = array[k][j];
+                }
+            }
+    printf("\n");
+    printf("First minimal element: %d", min);
+
+    int min_1 = array[1][0];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            if (i > j && i < N - j - 1) {
+                if (array[i][j] < min_1) {
+                    min_1 = array[i][j];
+                }
+            }
+    printf("\n");
+    printf("Second minimal element: %d", min_1);
+    printf("\n");
+    system("pause");
+}
+
+//some condition :3
+int fifth() {
+    int array[5][5];
+}
+
 int main() {
     int choose = 0;
-    printf("1 / 2 / 3: ");
+    printf("1 / 2 / 3 / 4: ");
     scanf("%d", &choose);
     if (choose == 1) {
         first();
@@ -197,6 +251,8 @@ int main() {
         second();
     } else if (choose == 3) {
         third();
+    } else if (choose == 4) {
+        fourth();
     } else {
         printf("Input Error");
     }
